886ef936308902378ec6a68dbd1e1adaad26a16a
AAKASH HANDA <AAKASH HANDA>
2017-03-18 22:20:30 +0530

    a

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+var connect = require('gulp-connect');
+var open = require('gulp-open');
+
+var config={
+	port:9005,
+	devBaseUrl:'http:localhost',
+	paths:{
+		html:'./src/*.html',
+		dist:'./dist'
+	}
+}
+
+gulp.task('connect',function(){
+	connect.server({
+		root:['dist'],
+		port:config.port,
+		base:config.devBaseUrl,
+		liverload:true
+	});
+})
+
+gulp.task('open',['connect'],function(){
+		gulp.src('dist/index.html')
+			.pipe(open({uri:config.devBaseUrl+':'+config.port+'/'}));
+})
